140c21da90734e9b4ebf7ce69557c3d47bc82d1d
Fabio Silva <fabiolindemberg@gmail.com>
2021-06-01 00:41:18 -0300

    apply Single Principle Responsability by change class Square and add SquareUI

--- solid.playground/Contents.swift
+++ solid.playground/Contents.swift
@@ -2,8 +2,8 @@ import UIKit
 
 class Square {
     let side = 5
-    var highResolutionMonitor = false
     
+    // These two methods were kept here because they have a high cohesion between them
     func calculateArea() -> Int {
         side * side
     }
@@ -11,7 +11,13 @@ class Square {
     func calculatePerimenter() -> Int {
         side * 4
     }
-    
+}
+
+var highResolutionMonitor = false
+
+class SquareUI {
+    // The methods draw and rotate are separeted in a new class SquareUI to keep the cohesion
+    // between these two methods
     func draw() {
         if highResolutionMonitor {
             // render a high resolution image of a square
