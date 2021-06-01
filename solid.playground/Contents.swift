1d4027aa5a58ca12dd69df54eff7f79d521ffed7
Fabio Silva <fabiolindemberg@gmail.com>
2021-06-01 00:33:15 -0300

    add class Square before applay Single Principle Responsability

--- solid.playground/Contents.swift
+++ solid.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Square {
+    let side = 5
+    var highResolutionMonitor = false
+    
+    func calculateArea() -> Int {
+        side * side
+    }
+    
+    func calculatePerimenter() -> Int {
+        side * 4
+    }
+    
+    func draw() {
+        if highResolutionMonitor {
+            // render a high resolution image of a square
+        } else {
+            // render a normal image of a square
+        }
+    }
+    
+    func rotate(degree: Int) {
+        // rotate the image of the square clockwise to
+        // the required degree and re-render
+    }
+}
